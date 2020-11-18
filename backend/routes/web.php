3400a371b7fd96f3099d5a5478462dd542012dfa
pkaratzhs <pkaratzhs@gmail.com>
2020-11-18 03:25:59 +0200

    Added fortify, sanctum and relevant configurations

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+$limiter = config('fortify.limiters.login');
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware(array_filter([
+    'guest',
+    $limiter ? 'throttle:'.$limiter : null,
+]));
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
